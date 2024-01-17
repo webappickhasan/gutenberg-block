bbf0ae7f35b10103c1c9ceeb220d07a7faacd1df
webappickhasan <azizulhasan.webappick@gmail.com>
2024-01-17 18:51:37 +0600

    test

--- gutenberg-block.php
+++ gutenberg-block.php
@@ -25,6 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function gutenberg_block_gutenberg_block_block_init() {
+
 	register_block_type( __DIR__ . '/build' );
 }
 
